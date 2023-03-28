4c654d146bc3b9c93cfd492f2f0c45fc3d56eb38
Dmitriy Bronnikov <dbron9269@gmail.com>
2023-03-28 16:14:54 +0400

    Basic project setup
    Basic project setup 2

--- SimpleChatApp/ViewController.swift
+++ SimpleChatApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let label: UILabel = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = "Hello, world!"
+        label.textColor = .systemRed
+        view.addSubview(label)
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        label.sizeToFit()
+        label.center = view.center
+    }
 }
 
